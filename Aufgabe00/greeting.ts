52f4f22efd2af532d43cbcc8aba363dd0c1106b3
Lisa <Lisa@DESKTOP-B9VRFTV.Speedport_W723_V_Typ_A_1_01_017>
2017-03-17 23:37:19 +0100

    greeting fix

--- Aufgabe00/greeting.ts
+++ Aufgabe00/greeting.ts
@@ -1,6 +1,6 @@
  document.addEventListener('DOMContentLoaded', function() {
     
-    var name = prompt("Wie heiﬂen Sie?");
+    var name = prompt("Geben Sie Ihren Namen ein");
     alert("Hallo "+ name + "!");
     
     
