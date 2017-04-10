0008f7d9e9a2a542a0884706869332508fb5305f
Lisa <meisterl@hs-furtwangen.de>
2017-04-10 11:56:19 +0200

    chess tooltip

--- Aufgabe02/chess.js
+++ Aufgabe02/chess.js
@@ -11,11 +11,12 @@ document.addEventListener('DOMContentLoaded', function () {
     let fields = 0;
     let line = 0;
     let rice = 1;
+    let box = document.getElementById("box");
     for (fields = 0; fields < 64; fields++) {
         let field = document.createElement("div");
         document.body.appendChild(field);
         field.style.border = "solid black";
-        field.innerText = "" + rice;
+        //        field.innerText = "" + rice;
         rice = rice * 2;
         if (fields % 8 == 0) {
             line = line + 1;
@@ -39,37 +40,39 @@ document.addEventListener('DOMContentLoaded', function () {
             }
         }
     }
+    console.log(box.innerText);
     document.getElementsByTagName("div")[0];
     let fieldList = document.getElementsByTagName("div");
     let clickedDiv;
     //Box, die sich mitbewegt
-    //    let box: HTMLElement = document.createElement("div");
-    //    document.body.appendChild(box);
-    //    let box: HTMLElement = document.getElementById("box");
-    //    box.innerText = "" + rice;
-    //    box.style.display = "none";
+    box.style.display = "none";
+    let i;
     for (let i = 0; i < 8; i++) {
         fieldList[i].addEventListener("click", selection);
+        box.innerText = "Dezimalzahl: " + rice + "Hexadezimalzahl: " + rice.toString(16);
     }
     function selection(_event) {
         clickedDiv = _event.target;
         console.log("border color = " + clickedDiv.style.border);
         if (clickedDiv.style.border == "solid black") {
             clickedDiv.style.border = "solid red";
-            console.log("border color = " + clickedDiv.style.border);
+            clickedDiv.addEventListener("mousemove", coordinates);
+            //            console.log("border color = " + clickedDiv.style.border);
+            //            rice = rice + Number(fieldList[i].textContent);
+            box.style.display = "inline-block";
         }
         else {
             clickedDiv.style.border = "solid black";
+            //            clickedDiv.addEventListener("mousemove", removeHandler);
+            box.style.display = "none";
         }
-        clickedDiv.addEventListener("mousemove", coordinates);
     }
     function removeHandler() {
         clickedDiv.removeEventListener('mousemove', coordinates);
     }
     function coordinates(_event) {
-        let x = _event.clientX;
-        let y = _event.clientY;
-        //        box.style.display = "block";
+        document.getElementById("box").style.left = (_event.clientX + 10) + "px";
+        document.getElementById("box").style.top = (_event.clientY + 10) + "px";
     }
 });
 //# sourceMappingURL=chess.js.map
--- Aufgabe02/chess.ts
+++ Aufgabe02/chess.ts
@@ -13,17 +13,19 @@ document.addEventListener('DOMContentLoaded', function() {
     let fields: number = 0;
     let line: number = 0;
     let rice: number = 1;
-
+     let box: HTMLElement = document.getElementById("box");
 
     for (fields = 0; fields < 64; fields++) {
-        
-        let field: HTMLElement = document.createElement("div");
-            
-            document.body.appendChild(field);
-            field.style.border = "solid black";
-            field.innerText = "" + rice;
-            rice = rice * 2;
 
+        let field: HTMLElement = document.createElement("div");
+
+        document.body.appendChild(field);
+        field.style.border = "solid black";
+//        field.innerText = "" + rice;
+        rice = rice * 2;
+       
+        
+        
 
         if (fields % 8 == 0) {
             line = line + 1;
@@ -48,20 +50,20 @@ document.addEventListener('DOMContentLoaded', function() {
     }
 
 
-    
+console.log(box.innerText);
     document.getElementsByTagName("div")[0];
     let fieldList: NodeListOf<HTMLElement> = document.getElementsByTagName("div");
     let clickedDiv: HTMLDivElement;
 
     //Box, die sich mitbewegt
-//    let box: HTMLElement = document.createElement("div");
-//    document.body.appendChild(box);
-//    let box: HTMLElement = document.getElementById("box");
-//    box.innerText = "" + rice;
-//    box.style.display = "none";
-
-    for (let i: number = 0; i < 8; i++) {
+  
+    
+    box.style.display = "none";
+    let i:number;
+    
+    for (let i = 0; i < 8; i++) {
         fieldList[i].addEventListener("click", selection);
+        box.innerText = "Dezimalzahl: " + rice + "Hexadezimalzahl: " + rice.toString(16);
     }
 
     function selection(_event: Event): void {
@@ -70,25 +72,34 @@ document.addEventListener('DOMContentLoaded', function() {
 
         if (clickedDiv.style.border == "solid black") {
             clickedDiv.style.border = "solid red";
-
-            console.log("border color = " + clickedDiv.style.border);
+            clickedDiv.addEventListener("mousemove", coordinates);
+//            console.log("border color = " + clickedDiv.style.border);
+//            rice = rice + Number(fieldList[i].textContent);
+            
+            box.style.display = "inline-block";
         }
+        
         else {
             clickedDiv.style.border = "solid black";
+//            clickedDiv.addEventListener("mousemove", removeHandler);
+            box.style.display = "none";
+//            rice = rice - Number(fieldList[i].textContent);
         }
-        clickedDiv.addEventListener("mousemove", coordinates);
+        
+        
     }
-    
+
     function removeHandler() {
         clickedDiv.removeEventListener('mousemove', coordinates);
     }
 
     function coordinates(_event: MouseEvent): void {
+        document.getElementById("box").style.left = (_event.clientX + 10) + "px";
+        document.getElementById("box").style.top = (_event.clientY + 10) + "px";
         
-        let x = _event.clientX;
-        let y = _event.clientY;
-//        box.style.display = "block";
         
+       
+            
     }
 
 
