ce139353276f09a33234b7a996651fecd91725e3
Lisa <meisterl@hs-furtwangen.de>
2017-05-29 21:14:55 +0200

    buttons fix

--- Aufgabe09/forms.js
+++ Aufgabe09/forms.js
@@ -8,21 +8,34 @@ var Form;
     let scoopPrice = 1;
     let toppingPrice = 0.4;
     let sum = 0;
+    let selectedFlavors = [];
+    let selectedToppings = [];
     function init(_event) {
         console.log("Init");
         let fieldsets = document.getElementsByTagName("fieldset");
+        //EventListener an fieldsets
         for (let i = 0; i < fieldsets.length; i++) {
             let fieldset = fieldsets[i];
             fieldset.addEventListener("change", handleChange);
         }
-        let scoopButtons = document.getElementsByClassName("addScoop");
-        for (let i = 0; i < scoopButtons.length; i++) {
-            let scoopButton = scoopButtons[i];
-            scoopButton.addEventListener("click", createFlavorField);
-        }
         createContainerField();
     }
     ;
+    //value der Nummernfelder sammeln
+    let numberFields = document.getElementsByName("scoopNumber");
+    for (let i = 0; i < numberFields.length; i++) {
+        let numberField = numberFields[i];
+    }
+    //ausgewählte Eissorten & Toppings in Cart schreiben
+    for (let i = 0; i < selectedFlavors.length; i++) {
+        document.getElementById("products").textContent = selectedFlavors[i];
+    }
+    for (let i = 0; i < selectedToppings.length; i++) {
+        document.getElementById("products").textContent = selectedToppings[i];
+    }
+    //Summe in Cart schreiben
+    sum = selectedFlavors.length * scoopPrice + selectedToppings.length * 0.4;
+    document.getElementById("total").textContent = "" + sum;
     function handleChange(_event) {
         //console.log(_event);
         //*/
@@ -51,52 +64,56 @@ var Form;
         containerField.id = "radio";
         let mainDiv = document.getElementById("main");
         mainDiv.appendChild(containerField);
+        //Legende für containerField
         let legend = document.createElement("legend");
         legend.innerText = "Cone or Cup?";
         containerField.appendChild(legend);
+        //Behälter-Optionen für Array-Einträge
         for (let i = 0; i < containers.length; i++) {
             let container = document.createElement("input");
             container.type = "checkbox";
             container.value = containers[i];
             container.name = "Checkbox" + i;
             containerField.appendChild(container);
+            //Labels für Behälterauswahl
             let containerLabel = document.createElement("label");
             containerLabel.textContent = containers[i];
             containerLabel.htmlFor = "radio" + i + 1;
             containerField.appendChild(containerLabel);
-            container.addEventListener("change", handleChange);
+            container.addEventListener("change", handleChange); //listener an Auswahl
         }
+        //scoopButton erstellen
         let scoopButton = document.createElement("button");
         scoopButton.type = "button";
         scoopButton.name = "ScoopButton";
         scoopButton.className = "addScoop";
         scoopButton.innerText = "Add Scoop";
         containerField.appendChild(scoopButton);
-        let scoopButtons = document.getElementsByClassName("addScoop");
-        for (let i = 0; i < scoopButtons.length; i++) {
-            let scoopButton = scoopButtons[i];
-            scoopButton.addEventListener("click", createFlavorField);
-        }
+        addListenerToScoopButton();
     }
     function createFlavorField() {
-        //        document.getElementById("flavors").style.display = "block";
+        //flavorField erstellen
         let flavorField = document.createElement("fieldset");
-        flavorField.id = "flavors";
+        flavorField.className = "flavorField";
         let mainDiv = document.getElementById("main");
         mainDiv.appendChild(flavorField);
+        //Legende
         let legend = document.createElement("legend");
         legend.innerText = "Choose Your Flavors";
         flavorField.appendChild(legend);
+        //Select-Form
         let flavorSelection = document.createElement("select");
         flavorSelection.name = "Select";
         flavorSelection.id = "flavorSelection";
         flavorField.appendChild(flavorSelection);
+        //Optionen für Array-Einträge
         for (let i = 0; i < flavors.length; i++) {
             let flavor = document.createElement("option");
             flavor.value = flavors[i];
             flavor.text = flavors[i];
             flavorSelection.appendChild(flavor);
         }
+        //Number-Feld für Eiskugel-Anzahl
         let scoopNumber = document.createElement("input");
         scoopNumber.type = "number";
         scoopNumber.name = "scoopNumber";
@@ -105,52 +122,56 @@ var Form;
         scoopNumber.max = "5";
         scoopNumber.value = "0";
         flavorField.appendChild(scoopNumber);
+        //toppingButton erstellen
         let toppingButton = document.createElement("button");
         toppingButton.type = "button";
         toppingButton.name = "ToppingButton";
         toppingButton.className = "addTopping";
         toppingButton.innerText = "Add Topping";
         flavorField.appendChild(toppingButton);
+        //EventListenser an toppingButtons
         let toppingButtons = document.getElementsByClassName("addTopping");
         for (let i = 0; i < toppingButtons.length; i++) {
             let toppingButton = toppingButtons[i];
             toppingButton.addEventListener("click", createToppingField);
         }
-        flavorSelection.addEventListener("change", handleChange);
-        scoopNumber.addEventListener("change", handleChange);
+        flavorSelection.addEventListener("change", handleChange); //eventListener an flavorSelect-Feld
+        scoopNumber.addEventListener("change", handleChange); //eventListener an scoopNumber-Feld
     } //createFlavorField
     function createToppingField() {
-        //        let toppings = document.getElementById("toppings");
-        //        var toppingsCopy = toppings.cloneNode(true);
-        //        document.getElementById("main").appendChild(toppingsCopy);
-        //        document.getElementById("toppi).style.display = "block";
+        //toppingField erstellen
         let toppingField = document.createElement("fieldset");
         toppingField.id = "toppings";
         let mainDiv = document.getElementById("main");
         mainDiv.appendChild(toppingField);
+        //Legende
         let legend = document.createElement("legend");
         legend.innerText = "Add Your Toppings";
         toppingField.appendChild(legend);
-        //        let scoopNumber = document.createElement("input");
-        //        scoopNumber.type = "checkbox";
+        //checkbox für Array-Einträge
         for (let i = 0; i < toppings.length; i++) {
             let topping = document.createElement("input");
             topping.type = "checkbox";
             topping.value = flavors[i];
             topping.name = "Checkbox" + i;
             toppingField.appendChild(topping);
+            //Label für checkboxen
             let toppingLabel = document.createElement("label");
             toppingLabel.textContent = toppings[i];
             toppingLabel.htmlFor = topping.name;
             toppingField.appendChild(toppingLabel);
             topping.addEventListener("change", handleChange);
         }
+        //scoopButton
         let scoopButton = document.createElement("button");
         scoopButton.type = "button";
         scoopButton.name = "ScoopButton";
         scoopButton.className = "addScoop";
         scoopButton.innerText = "Add Scoop";
         toppingField.appendChild(scoopButton);
+        addListenerToScoopButton();
+    }
+    function addListenerToScoopButton() {
         let scoopButtons = document.getElementsByClassName("addScoop");
         for (let i = 0; i < scoopButtons.length; i++) {
             let scoopButton = scoopButtons[i];
--- Aufgabe09/forms.ts
+++ Aufgabe09/forms.ts
@@ -9,31 +9,42 @@ namespace Form {
     let scoopPrice: number = 1;
     let toppingPrice: number = 0.4;
     let sum: number = 0;
+    let selectedFlavors: string[] = [];
+    let selectedToppings: string[] = [];
+
 
     function init(_event: Event): void {
         console.log("Init");
         let fieldsets: NodeListOf<HTMLFieldSetElement> = document.getElementsByTagName("fieldset");
 
-
+        //EventListener an fieldsets
         for (let i: number = 0; i < fieldsets.length; i++) {
             let fieldset: HTMLFieldSetElement = fieldsets[i];
             fieldset.addEventListener("change", handleChange);
         }
-
-
-        let scoopButtons: NodeListOf<Element> = document.getElementsByClassName("addScoop");
-
-        for (let i: number = 0; i < scoopButtons.length; i++) {
-            let scoopButton = scoopButtons[i];
-            scoopButton.addEventListener("click", createFlavorField);
-        }
-
         createContainerField();
     };
 
 
+    //value der Nummernfelder sammeln
+    let numberFields = document.getElementsByName("scoopNumber");
+    for (let i: number = 0; i < numberFields.length; i++) {
+        let numberField = numberFields[i];
+
+    }
 
 
+    //ausgewählte Eissorten & Toppings in Cart schreiben
+    for (let i: number = 0; i < selectedFlavors.length; i++) {
+        document.getElementById("products").textContent = selectedFlavors[i];
+    }
+    for (let i: number = 0; i < selectedToppings.length; i++) {
+        document.getElementById("products").textContent = selectedToppings[i];
+    }
+
+    //Summe in Cart schreiben
+    sum = selectedFlavors.length * scoopPrice + selectedToppings.length * 0.4;
+    document.getElementById("total").textContent = "" + sum;
 
     function handleChange(_event: Event): void {
         //console.log(_event);
@@ -67,10 +78,12 @@ namespace Form {
         let mainDiv = document.getElementById("main");
         mainDiv.appendChild(containerField);
 
+        //Legende für containerField
         let legend = document.createElement("legend");
         legend.innerText = "Cone or Cup?";
         containerField.appendChild(legend);
 
+        //Behälter-Optionen für Array-Einträge
         for (let i: number = 0; i < containers.length; i++) {
             let container = document.createElement("input");
             container.type = "checkbox";
@@ -78,50 +91,45 @@ namespace Form {
             container.name = "Checkbox" + i;
             containerField.appendChild(container);
 
+            //Labels für Behälterauswahl
             let containerLabel = document.createElement("label");
             containerLabel.textContent = containers[i];
             containerLabel.htmlFor = "radio" + i + 1;
             containerField.appendChild(containerLabel);
-            container.addEventListener("change", handleChange);
-
+            container.addEventListener("change", handleChange);//listener an Auswahl
         }
+
+        //scoopButton erstellen
         let scoopButton = document.createElement("button");
         scoopButton.type = "button";
         scoopButton.name = "ScoopButton";
         scoopButton.className = "addScoop";
-
         scoopButton.innerText = "Add Scoop";
         containerField.appendChild(scoopButton);
 
-
-        let scoopButtons: NodeListOf<Element> = document.getElementsByClassName("addScoop");
-
-        for (let i: number = 0; i < scoopButtons.length; i++) {
-            let scoopButton = scoopButtons[i];
-            scoopButton.addEventListener("click", createFlavorField);
-        }
-
+        addListenerToScoopButton();
     }
 
 
     function createFlavorField(): void {
-        //        document.getElementById("flavors").style.display = "block";
-
-
+        //flavorField erstellen
         let flavorField = document.createElement("fieldset");
-        flavorField.id = "flavors";
+        flavorField.className = "flavorField";
         let mainDiv = document.getElementById("main");
         mainDiv.appendChild(flavorField);
 
+        //Legende
         let legend = document.createElement("legend");
         legend.innerText = "Choose Your Flavors";
         flavorField.appendChild(legend);
 
+        //Select-Form
         let flavorSelection = document.createElement("select");
         flavorSelection.name = "Select";
         flavorSelection.id = "flavorSelection";
         flavorField.appendChild(flavorSelection);
 
+        //Optionen für Array-Einträge
         for (let i: number = 0; i < flavors.length; i++) {
             let flavor = document.createElement("option");
             flavor.value = flavors[i];
@@ -129,6 +137,7 @@ namespace Form {
             flavorSelection.appendChild(flavor);
         }
 
+        //Number-Feld für Eiskugel-Anzahl
         let scoopNumber = document.createElement("input");
         scoopNumber.type = "number";
         scoopNumber.name = "scoopNumber";
@@ -138,45 +147,38 @@ namespace Form {
         scoopNumber.value = "0";
         flavorField.appendChild(scoopNumber);
 
+        //toppingButton erstellen
         let toppingButton = document.createElement("button");
         toppingButton.type = "button";
         toppingButton.name = "ToppingButton";
         toppingButton.className = "addTopping";
-
         toppingButton.innerText = "Add Topping";
         flavorField.appendChild(toppingButton);
 
-
+        //EventListenser an toppingButtons
         let toppingButtons: NodeListOf<Element> = document.getElementsByClassName("addTopping");
-
         for (let i: number = 0; i < toppingButtons.length; i++) {
             let toppingButton = toppingButtons[i];
             toppingButton.addEventListener("click", createToppingField);
         }
-
-
-
-        flavorSelection.addEventListener("change", handleChange);
-        scoopNumber.addEventListener("change", handleChange);
+        flavorSelection.addEventListener("change", handleChange);//eventListener an flavorSelect-Feld
+        scoopNumber.addEventListener("change", handleChange);//eventListener an scoopNumber-Feld
     }//createFlavorField
 
     function createToppingField(): void {
-        //        let toppings = document.getElementById("toppings");
-        //        var toppingsCopy = toppings.cloneNode(true);
-        //        document.getElementById("main").appendChild(toppingsCopy);
-        //        document.getElementById("toppi).style.display = "block";
 
+        //toppingField erstellen
         let toppingField = document.createElement("fieldset");
         toppingField.id = "toppings";
         let mainDiv = document.getElementById("main");
         mainDiv.appendChild(toppingField);
 
+        //Legende
         let legend = document.createElement("legend");
         legend.innerText = "Add Your Toppings";
         toppingField.appendChild(legend);
 
-        //        let scoopNumber = document.createElement("input");
-        //        scoopNumber.type = "checkbox";
+        //checkbox für Array-Einträge
         for (let i: number = 0; i < toppings.length; i++) {
             let topping = document.createElement("input");
             topping.type = "checkbox";
@@ -184,7 +186,7 @@ namespace Form {
             topping.name = "Checkbox" + i;
             toppingField.appendChild(topping);
 
-
+            //Label für checkboxen
             let toppingLabel = document.createElement("label");
             toppingLabel.textContent = toppings[i];
             toppingLabel.htmlFor = topping.name;
@@ -192,22 +194,24 @@ namespace Form {
             topping.addEventListener("change", handleChange);
         }
 
-let scoopButton = document.createElement("button");
+        //scoopButton
+        let scoopButton = document.createElement("button");
         scoopButton.type = "button";
         scoopButton.name = "ScoopButton";
         scoopButton.className = "addScoop";
-
         scoopButton.innerText = "Add Scoop";
         toppingField.appendChild(scoopButton);
 
+        addListenerToScoopButton();
+    }
 
+    function addListenerToScoopButton(): void {
         let scoopButtons: NodeListOf<Element> = document.getElementsByClassName("addScoop");
 
         for (let i: number = 0; i < scoopButtons.length; i++) {
             let scoopButton = scoopButtons[i];
             scoopButton.addEventListener("click", createFlavorField);
         }
-
     }
 
 } //namespace
