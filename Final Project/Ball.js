d53ef0c232ccdbadff2aa16a7a0aac29b7695a06
Lisa <meisterl@hs-furtwangen.de>
2017-07-15 16:38:35 +0200

    Bar collision fix!

--- Final Project/Ball.js
+++ Final Project/Ball.js
@@ -4,7 +4,7 @@ var Bricks;
         constructor() {
             console.log("Ball");
             this.x = Bricks.crc2.canvas.width / 2;
-            this.y = Bricks.crc2.canvas.height - 30;
+            this.y = Bricks.crc2.canvas.height - 60;
             this.xd = 2;
             this.yd = -2;
             this.radius = 10;
@@ -30,27 +30,45 @@ var Bricks;
             if (this.y + this.yd < this.radius) {
                 this.yd = -this.yd; //nach unten bewegen --> Vorzeichen von yd zu +
             }
-            else if (this.y + this.yd > Bricks.crc2.canvas.height - this.radius) {
-                //Balken getroffen
-                if (this.x > Bricks.bar.x && this.x < Bricks.bar.x + Bricks.bar.width) {
-                    this.yd = -this.yd; //wieder nach oben
-                }
-                else {
-                    Bricks.crc2.fillStyle = "#FFFFFF";
-                    Bricks.crc2.fillRect(0, 0, Bricks.crc2.canvas.width, Bricks.crc2.canvas.height);
-                    Bricks.crc2.closePath();
-                    Bricks.crc2.font = "50px Arial";
-                    Bricks.crc2.fillStyle = "#000000";
-                    Bricks.crc2.fillText("GAME OVER", 100, 100);
-                    //restart game after 3 seconds
-                    setTimeout(function () {
-                        document.location.reload();
-                    }, 3000);
-                }
+            //vom Balken abprallen
+            if (this.y > Bricks.bar.y - this.radius && this.x > Bricks.bar.x && this.x < Bricks.bar.x + Bricks.bar.width) {
+                this.yd = -this.yd;
             }
+            //unterer Rand erreicht
+            if (this.y + this.yd > Bricks.crc2.canvas.height - this.radius) {
+                console.log("game over");
+                Bricks.crc2.fillStyle = "#FF0000";
+                Bricks.crc2.fillRect(0, 0, Bricks.crc2.canvas.width, Bricks.crc2.canvas.height);
+                Bricks.crc2.font = "50px Arial";
+                Bricks.crc2.fillStyle = "#000000";
+                Bricks.crc2.fillText("GAME OVER", 100, 100);
+            }
+            //                //unterer Rand erreicht
+            //            } else if (this.y + this.yd > crc2.canvas.height - this.radius) {
+            //
+            //                //Balken getroffen
+            //                if (this.x > bar.x && this.x < bar.x + bar.width ) {//&& this.y<barY
+            //                    this.yd = -this.yd; //wieder nach oben
+            //
+            //                    //Überschreitung unterer Rand GAME OVER
+            //                } else {
+            //                    crc2.fillStyle = "#FFFFFF";
+            //                    crc2.fillRect(0, 0, crc2.canvas.width, crc2.canvas.height);
+            //                    crc2.closePath();
+            //
+            //                    crc2.font = "50px Arial";
+            //                    crc2.fillStyle = "#000000";
+            //                    crc2.fillText("GAME OVER", 100, 100);
+            //
+            //                    //restart game after 3 seconds
+            //                    setTimeout(function() {
+            //                        document.location.reload();
+            //                    }, 3000);
+            //                }
+            //            }
             //neue Position
-            this.x += this.xd;
-            this.y += this.yd;
+            this.x += this.xd; //+2
+            this.y += this.yd; //-2
         } //move
     }
     Bricks.Ball = Ball; //class
--- Final Project/Ball.ts
+++ Final Project/Ball.ts
@@ -1,25 +1,22 @@
 namespace Bricks {
 
-
     export class Ball {
         x: number;
         y: number;
         xd: number;
         yd: number;
         color: string;
-        //        speed: number;
         radius: number;
 
 
         constructor() {
             console.log("Ball");
             this.x = crc2.canvas.width / 2;
-            this.y = crc2.canvas.height - 30;
+            this.y = crc2.canvas.height - 60;
             this.xd = 2;
             this.yd = -2;
             this.radius = 10;
         }
-
 
         update(): void {
             this.move();
@@ -34,8 +31,6 @@ namespace Bricks {
             crc2.closePath();
         }
 
-
-
         move(): void {
             let newX=this.x+this.xd;
 
@@ -47,34 +42,51 @@ namespace Bricks {
             //oberer Rand erreicht
             if (this.y + this.yd < this.radius) {
                 this.yd = -this.yd; //nach unten bewegen --> Vorzeichen von yd zu +
-
-                //unterer Rand erreicht
-            } else if (this.y + this.yd > crc2.canvas.height - this.radius) {
-
-                //Balken getroffen
-                if (this.x > bar.x && this.x < bar.x + bar.width ) {//&& this.y<barY
-                    this.yd = -this.yd; //wieder nach oben
-
-                    //Überschreitung unterer Rand GAME OVER
-                } else {
-                    crc2.fillStyle = "#FFFFFF";
-                    crc2.fillRect(0, 0, crc2.canvas.width, crc2.canvas.height);
-                    crc2.closePath();
-
-                    crc2.font = "50px Arial";
+            }
+            
+            //vom Balken abprallen
+            if (this.y>bar.y-this.radius && this.x>bar.x && this.x<bar.x+bar.width){
+                this.yd = -this.yd;
+            }
+            
+            //unterer Rand erreicht
+            if (this.y + this.yd > crc2.canvas.height - this.radius) {
+                console.log("game over");
+                crc2.fillStyle = "#FF0000";
+                crc2.fillRect(0, 0, crc2.canvas.width, crc2.canvas.height);
+                crc2.font = "50px Arial";
                     crc2.fillStyle = "#000000";
                     crc2.fillText("GAME OVER", 100, 100);
-
-                    //restart game after 3 seconds
-                    setTimeout(function() {
-                        document.location.reload();
-                    }, 3000);
                 }
-            }
+            
+            
+//                //unterer Rand erreicht
+//            } else if (this.y + this.yd > crc2.canvas.height - this.radius) {
+//
+//                //Balken getroffen
+//                if (this.x > bar.x && this.x < bar.x + bar.width ) {//&& this.y<barY
+//                    this.yd = -this.yd; //wieder nach oben
+//
+//                    //Überschreitung unterer Rand GAME OVER
+//                } else {
+//                    crc2.fillStyle = "#FFFFFF";
+//                    crc2.fillRect(0, 0, crc2.canvas.width, crc2.canvas.height);
+//                    crc2.closePath();
+//
+//                    crc2.font = "50px Arial";
+//                    crc2.fillStyle = "#000000";
+//                    crc2.fillText("GAME OVER", 100, 100);
+//
+//                    //restart game after 3 seconds
+//                    setTimeout(function() {
+//                        document.location.reload();
+//                    }, 3000);
+//                }
+//            }
 
             //neue Position
-            this.x += this.xd;
-            this.y += this.yd;
+            this.x += this.xd; //+2
+            this.y += this.yd; //-2
         }//move
     }//class
 
